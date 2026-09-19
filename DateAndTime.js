f213a2e2a9d7d10e0734e05367265a590faad183
Sameer Alam <sameeralam316@gmail.com>
2026-09-19 15:52:36 +0530

    done

---
+++ DateAndTime.js
@@ -0,0 +1,41 @@
+let now = new Date()
+// console.log(now);
+
+// console.log(now.getDate());
+// console.log(now.getMonth()+1);
+// console.log(now.getFullYear());
+// console.log(now.getDay());
+
+
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+
+
+let Barthday = new Date(2004,10,10)
+// console.log(Barthday);
+// console.log(Date.now());
+
+console.log(now.toDateString());
+console.log(now.toTimeString());
+console.log(now.toISOString());
+
+// all method
+// new Date()
+
+// Date.now()
+
+// getDate()
+// getDay()
+// getMonth()
+// getFullYear()
+
+// getHours()
+// getMinutes()
+// getSeconds()
+
+// getTime()
+
+// toDateString()
+// toTimeString()
+// toISOString()
